1a03d4e4fe4c55967f528bb1ee8ff28648803e64
Crzq <41958119+Crzq@users.noreply.github.com>
2022-05-09 17:00:42 +0800

    Add files via upload

---
+++ 05.09/results.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Information</title>
+    <style>
+        #pleaseLogin {
+            margin-top: 40px;
+            text-align: center;
+            
+        }
+    </style>
+</head>
+<body>
+<?php
+include 'header.html';
+?>
+
+<div id="pleaseLogin">
+    <a href="login.php?frompage=results">请先登录</a>
+    
+</div>
+
+<?php
+include 'footer.html';
+?>
+</body>
+</html>
+
